1ea1fe279a6ea0935c45bb8d16e600a5baa9d349
DARKYYXX <ashikuthuman286@gmail.com>
2024-10-25 10:33:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from program import Client, filters
 
+
+API_ID = ""
+API_HASH = ""
+BOT_TOKEN = ""
+
+Darky = Client(
+    name="Darkyxbot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+
+print("Bot was Started")
+Darky.run()
