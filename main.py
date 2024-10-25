a4b27b01e77527ce70688d1445b4b0fa8d857e6b
DARKYYXX <ashikuthuman286@gmail.com>
2024-10-25 14:18:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ API_HASH = "8ff4733c3bf0f52d2321ca2215fadd21"
 BOT_TOKEN = "8196994557:AAGw0-ZWF4LcqivO0qUs0_vfVdFS9yhk0vc"
 
 Darky = Client(
-    name="Darkyxbot",
+    name="PRIVATEDARKY",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN
@@ -22,7 +22,6 @@ async def hacks_cmd(client, message):
 @darky.on_message(filters.command("HACKS"))
 async def HACKS_cmd(client, message):
     await message.reply_text("must join this for hacks https://t.me/+prQ8IhRBLmZmODY1")
-
 
 
 print("Bot was Started")
